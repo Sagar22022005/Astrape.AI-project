383368411d18483caa4553cb5c6e31d9203e5e05
Jangam Sagar <sagar@example.com>
2025-09-08 02:43:17 +0530

    cc

--- frontend/e-commeroce/src/main.jsx
+++ frontend/e-commeroce/src/main.jsx
@@ -1,3 +1,7 @@
+window.alert = function () {
+  console.log("Blocked alert:", arguments);
+};
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
